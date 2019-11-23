214ab307b46d1ecc8a8af1fa38e4290fbfe8436c
dherv <damien.herv@gmail.com>
2019-11-23 12:48:29 +0900

    feature(watchlist) - Add card to watchlist page

--- src/pages/watchlist.tsx
+++ src/pages/watchlist.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { getMovies } from "../graphql/queries/queries";
 import { IMovie } from "../interfaces/Movie";
+import Card, { cardLandscapeSmall } from "../components/card/Card";
+import styled from "styled-components";
 
 const Watchlist = () => {
   const displayMovies = () => {
@@ -10,16 +12,32 @@ const Watchlist = () => {
       <div>loading movies...</div>
     ) : (
       data.movies.map((item: IMovie) => (
-        <li key={item.id}>{item.original_title}</li>
+        <CardContainer key={item.id}>
+          <Card
+            movie={item}
+            theme={cardLandscapeSmall}
+            parentPage="watchlist"
+          ></Card>
+        </CardContainer>
       ))
     );
   };
   return (
-    <div>
-      <h1>Movie</h1>
-      <ul>{displayMovies()}</ul>
-    </div>
+    <Container>
+      <List>{displayMovies()}</List>
+    </Container>
   );
 };
 
+const Container = styled.div`
+  width: 70vw;
+  margin: 2rem auto;
+`;
+const List = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`;
+const CardContainer = styled.div`
+  margin: 2rem;
+`;
 export default Watchlist;
