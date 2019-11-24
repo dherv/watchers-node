a379d3c3e9704dc80c85c70f368eeae1f6880dfa
dherv <damien.herv@gmail.com>
2019-11-24 15:37:02 +0900

    (develop) - Add generic url for apollo client

--- src/graphql/lib/withApollo.ts
+++ src/graphql/lib/withApollo.ts
@@ -1,11 +1,10 @@
 import withApollo from "next-with-apollo";
 import ApolloClient, { InMemoryCache } from "apollo-boost";
-// import { GRAPHQL_URL } from '../configs';
 
 export default withApollo(
   ({ initialState }) =>
     new ApolloClient({
-      uri: "http://localhost:3000/graphql",
+      uri: "/graphql",
       cache: new InMemoryCache().restore(initialState || {})
     })
 );
